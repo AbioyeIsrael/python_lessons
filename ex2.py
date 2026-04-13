a84fdf1074be8e2e4abeaea0a50ec46e3d6b8268
AbioyeIsrael <abioyeisrael39@gmail.com>
2026-04-13 01:36:43 +0100

    "Missing Track"

---
+++ ex2.py
@@ -0,0 +1,17 @@
+# Write your code below this line 👇
+print("Day 1 = Python Print Fucntion")
+print("The Function is declared like this:")
+print("print('What to Print')")
+print("Hello world\nHello World\nHelp World")
+print("Hello"+" Israel")
+print("Hello " " Universe!,\n this id the begining of my journey!\n to becomin g a great programmer !")
+#print('Hello' + input("what is your name\n what is your full name\n what is your nationality\n what is your date of birth\n what is the name of your school"))
+name = "Abioye Israel"
+print(name)
+name = "Abioye Abraham"
+print(name)
+name = input("what is your name?")
+lenght = len(name.replace(" ", ""))
+print(lenght)
+print("Hello " + name + " your name has " + str(lenght) + " characters")
+
---
+++ ex9.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+name = input("what is your name" )
+print(len(name))
+
+
+
+
+
+
+
+
+
